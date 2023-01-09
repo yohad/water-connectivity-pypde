35e055a6016d6aa7f676effad935bec6e62b5dc1
Yotam Ohad <yohad@users.noreply.github.com>
2023-01-09 16:08:46 +0200

    Change folder structure to be a python library

--- water_connectivity/models/precipitation.py
+++
@@ -1,5 +0,0 @@
-def const_precipitation(value=0.5):
-    def func(_):
-        return value
-
-    return func
--- water_connectivity/utils/math.py
+++
@@ -1,8 +0,0 @@
-from scipy import ndimage
-
-
-def laplace_periodic(u):
-    """
-    Laplace operator on a 2D matrix with periodic boundary conditions
-    """
-    return ndimage.laplace(u, mode='wrap')
